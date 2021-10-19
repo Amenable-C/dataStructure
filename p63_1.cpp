b7c0b7885bd5eda26d348da067f1a9035a9f3356
Amenable <duswo0624@naver.com>
2021-10-19 23:58:54 +0900

    second trial

---
+++ p63_1.cpp
@@ -0,0 +1,23 @@
+//#pragma once
+////Stack node& Queue Node
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//template <class T>
+//class Stack; //forward declaration
+//template <class T>
+//class Queue;
+//template <class T>
+//class StackNode {
+//	friend class Stack<T>;
+//private:
+//	T data;
+//	StackNode<T>* link;
+//public:
+//	StackNode<T>(T d = 0, StackNode<T>* l = 0) : data(d), link(l){} //constructor
+//	//StackNode<T>(T d = 0, StackNode<T>* l = 0) {
+//	//	data = d;
+//	//	link = l;
+//	//}//constructor
+//
+//};
