5c0063593ccff132de3fd29bc31ffd2215ed1fb6
Amenable <duswo0624@naver.com>
2021-10-28 23:09:10 +0900

    1_11

---
+++ sourceCode_1_11.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//void Fibonacci(int*, int);
+//int main() {
+//	int Fibo[1000];
+//	const int maxIndex = 1000;
+//	for (int i = 0; i < maxIndex; i++)
+//		Fibo[i] = 0;
+//	Fibonacci(Fibo, maxIndex);
+//	cout << endl;
+//}
+//void Fibonacci(int a[], int rows) {
+//	if (rows <= 1)
+//		cout << rows << endl;
+//	else {
+//		// a[i + 2] = a[i + 1] + a[i] + a[i - 1];
+//	}
+//}
